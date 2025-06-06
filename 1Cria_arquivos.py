6205dcea2adb189d58cb226184d88025b5434f72
cleverson-moura <cleversonmoura178@gmail.com>
2025-06-06 11:55:56 -0300

    Corrigindo caminhos

--- 1Cria_arquivos.py
+++ 1Cria_arquivos.py
@@ -9,7 +9,7 @@ arquivo_users.close()
 
 # Arquivo para os dados dos administradores: #
 
-arquivo_adms = open('adms.txt', 'w')
+arquivo_adms = open('./registros/adms.txt', 'w')
 arquivo_adms.write('Administradores\n\n')
 arquivo_adms.close()
 
@@ -17,7 +17,7 @@ arquivo_adms.close()
 
 # Arquivo para os dados dos hoteis: #
 
-arquivo_hoteis = open('hoteis.txt', 'w')
+arquivo_hoteis = open('./registros/hoteis.txt', 'w')
 arquivo_hoteis.write('Hoteis\n\n')
 arquivo_hoteis.close()
 
@@ -25,7 +25,7 @@ arquivo_hoteis.close()
 
 # Arquivo para os dados dos pontos turísticos: #
 
-arquivo_pontos_turisticos = open('pontos_turisticos.txt', 'w')
+arquivo_pontos_turisticos = open('./registros/pontos_turisticos.txt', 'w')
 arquivo_pontos_turisticos .write('Pontos Turísticos\n\n')
 arquivo_pontos_turisticos .close()
 
@@ -33,7 +33,7 @@ arquivo_pontos_turisticos .close()
 
 # Arquivo para os dados dos quartos: #
 
-arquivo_quartos = open('quartos.txt', 'w')
+arquivo_quartos = open('./registros/quartos.txt', 'w')
 arquivo_quartos .write('Quartos\n\n')
 arquivo_quartos .close()
 
@@ -41,7 +41,7 @@ arquivo_quartos .close()
 
 # Arquivo para os dados das reservas: #
 
-arquivo_reservas = open('reservas.txt', 'w')
+arquivo_reservas = open('./registros/reservas.txt', 'w')
 arquivo_reservas .write('Reservas\n\n')
 arquivo_reservas .close()
 
