74982f23150bf4b0eeb5009ee81e8c702914da6a
cleverson-moura <cleversonmoura178@gmail.com>
2025-06-10 09:18:09 -0300

    Ajuste em cancelar reserva

--- models/reserva_model.py
+++ models/reserva_model.py
@@ -2,7 +2,8 @@ from models.connect import Database
 
 class ReservaModel:
     #futuramente adicionar data_entrada e data_saida
-    def __init__(self, id_usuario=None, id_quarto=None, id_hotel=None, tempo_estadia=None):
+    def __init__(self,id_reserva=None, id_usuario=None, id_quarto=None, id_hotel=None, tempo_estadia=None):
+        self.id_reserva = id_reserva
         self.id_usuario = id_usuario
         self.id_quarto = id_quarto
         self.id_hotel = id_hotel
@@ -16,3 +17,11 @@ class ReservaModel:
         reservas = db.fetchall()
         db.close()
         return reservas
+    
+    def cancelar_reserva(self):
+        db = Database()
+        db.connect()
+        sql = "DELETE FROM reservas WHERE id_reserva=?"
+        db.execute(sql, (self.id_reserva,))
+        db.commit()
+        db.close()
