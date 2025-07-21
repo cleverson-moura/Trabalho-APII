0668cd6edffc28df486264ed9841bfb43d79f8fb
Iruan12 <iruanmega@gmail.com>
2025-07-21 16:34:15 -0300

    As reservas funcionam e não dá para pegar data ocupada

--- routes/route_quartos.py
+++ routes/route_quartos.py
@@ -4,6 +4,7 @@ from models.quarto_model import QuartoModel
 from models.hotel_model import HotelModel
 from models.reserva_model import ReservaModel
 from datetime import datetime, timedelta
+import json
 
 quarto_bp = Blueprint('quarto', __name__, template_folder='../templates')
 
@@ -30,11 +31,12 @@ def quartos_reserva(id_hotel, id_quarto):
 
     reserva_model = ReservaModel(id_quarto=id_quarto)
     reservas = reserva_model.buscar_todas_reservas()
-
+    
     datas_ocupadas = []
     for reserva in reservas:
-        inicio = datetime.strptime(reserva['check_in'], "%Y-%m-%d")
-        fim = datetime.strptime(reserva['check_out'], "%Y-%m-%d")
+        inicio = datetime.strptime(reserva['data_checkin'], "%Y-%m-%d")
+        fim = datetime.strptime(reserva['data_checkout'], "%Y-%m-%d")
+
         while inicio <= fim:
             datas_ocupadas.append(inicio.strftime("%Y-%m-%d"))
             inicio += timedelta(days=1)
@@ -48,10 +50,10 @@ def quartos_reserva(id_hotel, id_quarto):
         if "usuario" in session:
             reserva = ReservaModel(None, session["usuario"]["id"], id_hotel, None, data_checkin, data_checkout)
             reserva.fazer_reserva()
-        if "hotel" in session:
+        elif "hotel" in session:
             reserva = ReservaModel(None, session["usuario"]["id"], id_hotel, None, data_checkin, data_checkout)
             reserva.fazer_reserva()
         else:
             return redirect(url_for("gerais.login"))
 
-    return render_template('quartos/quartos_hotel.html', icone=icone, endereco=endereco, hotel=hotel, quarto=quarto, rota=rota, botao_reserva_texto=botao_reserva_texto)
+    return render_template('quartos/quartos_hotel.html', icone=icone, endereco=endereco, hotel=hotel, quarto=quarto, rota=rota, botao_reserva_texto=botao_reserva_texto, datas_ocupadas=json.dumps(datas_ocupadas))
