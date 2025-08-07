0e0b0086ed369b0635c40435ccf87a41648ae30e
Carla-newDEV <carla.mariana.t.a.s.o@gmail.com>
2025-08-07 12:35:48 -0300

    Criação dos models das avaliações dos hoteis e dos quartos

---
+++ models/avaliacao_quarto_model.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+class AvaliacaoQuartoModel:
+    @staticmethod
+    def inserir_avaliacao(id_usuario, id_hotel, id_quarto, estrelas, comentario):
+        try:
+            con = sqlite3.connect("database/banco/SITE.db")
+            cursor = con.cursor()
+            cursor.execute('''INSERT INTO avaliacoes_quartos(id_usuario, id_hotel, id_quarto, estrelas, comentario) VALUES(?,?,?,?,?)''', (id_usuario, id_hotel, id_quarto, estrelas, comentario))
+            con.commit()
+            return True
+        except sqlite3.IntegrityError:
+            return False
+        finally:
+            con.close()
+
+    @staticmethod
+    def buscar_avaliacoes_quarto(id_hotel, id_quarto):
+        con = sqlite3.connect("database/banco/SITE.db")
+        cursor = con.cursor()
+        cursor.execute('''SELECT u.nome, a.estrelas, a.comentario, a.data_avaliacao
+                       FROM avaliacoes_quartos a
+                       JOIN usuarios u ON a.id_usuario = u.id_usuario
+                       WHERE a.id_hotel = ? AND a.id_quarto = ?
+                       ORDER BY a.data_avaliacao DESC
+                       ''', (id_hotel, id_quarto))
+        avaliacoes = cursor.fetchall()
+        con.close()
+        return avaliacoes
+
+    @staticmethod
+    def calcular_media_estrelas(id_hotel, id_quarto):
+        con = sqlite3.connect("database/banco/SITE.db")
+        cursor = con.cursor()
+        cursor.execute('''SELECT AVG(estrelas), COUNT(*) FROM avaliacoes_quartos 
+                        WHERE id_hotel = ? AND id_quarto = ?
+                        ''', (id_hotel, id_quarto))
+        media, total = cursor.fetchone()
+        con.close()
+        return (round(media, 1) if media else 0, total)
